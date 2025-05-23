de194b2cfe3c055ef16aac90e23138098f1002dd
Yash Solanki <yashns2013@gmail.com>
2025-05-23 18:09:50 +0530

    python bool type
    Signed-off-by: Yash Solanki <yashns2013@gmail.com>

---
+++ 20250523_bool.py
@@ -0,0 +1,16 @@
+print("this works for < ")
+
+
+
+
+g=int(input("enter numeric value of g: "))
+n=int(input("enter numeric value of n: "))
+
+lessthan = g<n
+greaterthan = g>n
+equals = g==n
+
+print("g < n = (", g, " < ", n, ") = ", lessthan)
+print("g > n = (", g, " > ", n, ") = ", greaterthan)
+print("g == n = (", g, " == ", n, ") = ", equals)
+
