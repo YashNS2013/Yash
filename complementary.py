6700ec728cfbfe177e1f564ace64698c1624c672
Yash Solanki <yashns2013@gmail.com>
2025-07-19 16:50:26 +0530

    20250719: Math practice
    Signed-off-by: Yash Solanki <yashns2013@gmail.com>

---
+++ complementary.py
@@ -0,0 +1,8 @@
+print("##################################################################")
+print("               welcome to the complementary.py program                 ")
+print("##################################################################")
+
+
+q=int(input("Enter the angle and I will find its complementary angle: "))
+print(90-q, "degrees is the complementary angle of", q, "degrees.")
+print("##################################################################")
---
+++ supplementary.py
@@ -0,0 +1,7 @@
+print("####################################################################")
+print("           welcome to the program supplementary.py           ")
+print("####################################################################")
+
+q=int(input("Enter the angle and I will find its supplementary angle: "))
+print(180-q, "degrees is the supplementary angle of", q, "degrees.")
+print("####################################################################")
